6d3ecd7b8e7dd855b36a648fe899a14972534d8b
Jesse-js <jesse-jjs@hotmail.com>
2024-03-11 23:28:10 -0300

    feat: add scaffold of the form

---
+++ resources/views/livewire/file-upload-form.blade.php
@@ -0,0 +1,24 @@
+<div class="d-flex flex-column align-items-center">
+    <div class="content d-inline-block mt-5 border p-5">
+        <h1 class="m-3 text-center border-bottom">Register Form</h1>
+        
+        <div class="mb-3">
+            <label for="name" class="form-label">Name:</label>
+            <input class="form-control" type="text" name="name" id="" placeholder="ex.: John Doe">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">E-mail:</label>
+            <input class="form-control" type="email" name="email" placeholder="ex.: jonh@foobar.com">
+        </div>
+        <div class="mb-3">
+            <label for="password">Password: </label>
+            <input class="form-control" type="password" name="password">
+        </div>
+        <div class="mb-3">
+            <label for="file">File: </label>
+            <input class="form-control" type="file" name="file">
+        </div>
+
+        <button type="submit" class="btn btn-primary mb-3">Submit</button>
+    </div>
+</div>
