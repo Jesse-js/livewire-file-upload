79fd692a2525cb123d46f42c4c94d189e950240c
Jesse-js <jesse-jjs@hotmail.com>
2024-03-11 23:33:25 -0300

    feat: add person model and migration

--- app/Livewire/FileUploadForm.php
+++ app/Livewire/FileUploadForm.php
@@ -2,11 +2,17 @@
 
 namespace App\Livewire;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class FileUploadForm extends Component
 {
-    public function render()
+    public string $name = '';
+    public string $email = '';
+    public string $password = '';
+
+
+    public function render(): View
     {
         return view('livewire.file-upload-form');
     }
