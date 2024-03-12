a169b02f89f1c4d87b85082cd8047c6a636ba2c0
Jesse-js <jesse-jjs@hotmail.com>
2024-03-11 23:46:16 -0300

    feat: add properties and validation

--- app/Livewire/FileUploadForm.php
+++ app/Livewire/FileUploadForm.php
@@ -2,15 +2,32 @@
 
 namespace App\Livewire;
 
+use App\Models\Person;
 use Illuminate\View\View;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class FileUploadForm extends Component
 {
+    #[Validate('required|min:3|max:150')]
     public string $name = '';
+
+    #[Validate('required|email|unique:people')]
     public string $email = '';
+
+    #[Validate('required|min:6|max:20')]
     public string $password = '';
 
+    public function submit(): void
+    {
+        $validated = $this->validate();
+
+        Person::create($validated);
+
+        $this->reset(['name', 'email', 'password']);
+
+        request()->session()->flash('success', 'Person created successfully!');
+    }
 
     public function render(): View
     {
--- resources/views/livewire/file-upload-form.blade.php
+++ resources/views/livewire/file-upload-form.blade.php
@@ -1,24 +1,38 @@
 <div class="d-flex flex-column align-items-center">
-    <div class="content d-inline-block mt-5 border p-5">
+    @if (session('success'))
+        <div class="alert alert-success mt-2" role="alert">
+            {{ session('success') }}
+        </div>
+    @endif
+    <div class="content d-inline-block mt-2 border p-5">
         <h1 class="m-3 text-center border-bottom">Register Form</h1>
         
         <div class="mb-3">
             <label for="name" class="form-label">Name:</label>
-            <input class="form-control" type="text" name="name" id="" placeholder="ex.: John Doe">
+            <input wire:model="name" class="form-control" type="text" name="name" id="" placeholder="ex.: John Doe">
+            @error('name')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">E-mail:</label>
-            <input class="form-control" type="email" name="email" placeholder="ex.: jonh@foobar.com">
+            <input wire:model="email" class="form-control" type="email" name="email" placeholder="ex.: jonh@foobar.com">
+            @error('email')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="password">Password: </label>
-            <input class="form-control" type="password" name="password">
+            <input wire:model="password" class="form-control" type="password" name="password">
+            @error('password')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="file">File: </label>
             <input class="form-control" type="file" name="file">
         </div>
 
-        <button type="submit" class="btn btn-primary mb-3">Submit</button>
+        <button wire:click="submit" type="submit" class="btn btn-primary mb-3">Submit</button>
     </div>
 </div>
